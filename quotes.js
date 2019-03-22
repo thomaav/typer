468b3d6a596b4568ae953ee24e8a8af4da9c6e70
Thomas Aven <avensin@hotmail.com>
2019-03-22 20:51:08 +0100

    Quotes.

---
+++ quotes.js
@@ -0,0 +1,3 @@
+var quotes = [
+    "And as the Strangefolk mined deeper and deeper into the mountain, holes began to appear. Bringing with them a cold and bitter wind that chilled the very soul of the Monkey. For the first time, the Happyfolk felt fearful, for they knew the Monkey would soon stir from its deep sleep. And then came a sound, distant first. It grew into castrophany, so immense it could be heard far away in space. There were no screams, there was no time. The mountain called Monkey had spoken, there was only fire. And then, nothing.",
+];
